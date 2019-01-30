9e7221e22aa44f69ffee89e1c952e79961610ea7
Harper Bellows <harper.bellows@gmail.com>
2019-01-30 16:32:47 -0700

    Add truncation method to seeds file to reset primary key to 1

--- db/seeds/dev/foods.js
+++ db/seeds/dev/foods.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('foods').del()
+  return knex.raw('TRUNCATE foods RESTART IDENTITY CASCADE')
     .then(function () {
       // Inserts seed entries
         return knex('foods').insert([
