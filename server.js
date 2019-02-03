3752713412b206ca46bc14e1059decd38e391485
William Fischer <william.fischer42@gmail.com>
2019-02-03 16:07:32 -0700

    Add patch to cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.locals.title = 'Quantified Self';
 // CORS Configuration
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
