e516ec3ca21da89ee7c4b6f5ebd54ca87fc1529c
Harper Bellows <harper.bellows@gmail.com>
2019-02-05 16:42:27 -0700

    Add an interim refactor to the create and destroy functions for meals -> needs further refactoring

--- server.js
+++ server.js
@@ -32,67 +32,6 @@ app.use('/api/v1/foods', foods)
 app.use('/api/v1/meals', meals)
 
 
-
-
-
-
-
-app.post('/api/v1/meals/:meal_id/foods/:id', (request, response) => {
-  const mealId = request.params.meal_id
-  const foodId = request.params.id
-
-  let targetMeal
-  let targetFood
-
-  database('meals').where('id', mealId).first()
-    .then(meal => {
-      targetMeal = meal
-      return database('foods').where('id', foodId).first()
-    })
-    .then(food => {
-      targetFood = food
-    })
-    .then(() => {
-      if (targetMeal && targetFood) {
-        return database('meal_foods').insert([{ food_id: foodId, meal_id: mealId }], 'id')
-      }
-    })
-    .then(() => {
-      response.status(201).json({ message: `Successfully added ${targetFood.name} to ${targetMeal.name}.` })
-    })
-    .catch((error) => {
-      response.status(400).json({ error })
-    })
-});
-
-app.delete('/api/v1/meals/:meal_id/foods/:id', (request,response) => {
-  const mealId = request.params.meal_id
-  const foodId = request.params.id
-
-  let targetMeal
-  let targetFood
-
-  database('meals').where('id', mealId).first()
-    .then(meal => {
-      targetMeal = meal
-      return database('foods').where('id', foodId).first()
-    })
-    .then(food => {
-      targetFood = food
-    })
-    .then(() => {
-      if (targetMeal && targetFood) {
-        return database('meal_foods').where('meal_foods.meal_id', mealId).where('meal_foods.food_id', foodId).del()
-      }
-    })
-    .then(() => {
-      response.status(200).json({ message: `Successfully removed ${targetFood.name} from ${targetMeal.name}.` })
-    })
-    .catch((error) => {
-      response.status(400).json({ error })
-    })
-});
-
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
